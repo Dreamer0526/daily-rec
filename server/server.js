3ae5270f8494fb6eb8601b0c062ef9eee42dde6c
Dreamer <xiemengzhu0526@gmail.com>
2018-11-25 12:53:16 +0800

    add folder for server; modify package.json for concurrently running client and server together

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/:name', (req, res) => {
+    res.send("HI, " + req.params.name)
+})
+
+app.listen(3001)
+console.log('Server starts listening to port 3001...')
